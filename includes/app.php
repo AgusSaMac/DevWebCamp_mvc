c9fd2b38d513ed8e61eb3fcfbb71184321547406
Agustin Sanchez Macal <agustin11089@gmail.com>
2023-01-31 18:31:16 -0600

    update

--- includes/app.php
+++ includes/app.php
@@ -1,15 +1,12 @@
 <?php 
 
 require __DIR__ . '/../vendor/autoload.php';
-use Dotenv\Dotenv;
-use Model\ActiveRecord;
-
-// Añadir Dotenv
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->safeLoad();
-
+$dotenv = Dotenv\Dotenv::CreateImmutable(__DIR__);
+$dotenv->safeload();
 require 'funciones.php';
 require 'database.php';
 
 // Conectarnos a la base de datos
+use Model\ActiveRecord;
+use Dotenv\Dotenv;
 ActiveRecord::setDB($db);
